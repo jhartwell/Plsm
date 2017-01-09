e4d641e8def419c1d110824701bd7a4247e66e9c
Jonathan Hartwell <jon@dontbreakthebuild.com>
2017-01-09 04:11:42 -0600

    Adding hex information

--- mix.exs
+++ mix.exs
@@ -7,15 +7,34 @@ defmodule Plasm.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
   def application do
-    [applications: [:logger]]
+    []
   end
 
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev} 
+    ]
+  end
+
+  defp description do
+    """
+      Plasm generates Ecto models based on existing database tables and populates the fields of the model.
+    """
+  end
+
+  defp package do
+    [
+     name: :plasm_ecto,
+     files: ["lib","mix.exs", "README.md", "LICENSE"],
+     maintainers: ["Jon Hartwell"],
+     licenses: ["MIT License"],
+     links: %{"GitHub" => "https://github.com/jhartwell/Plasm"}
     ]
   end
 end
