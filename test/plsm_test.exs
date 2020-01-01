e201f6d624aca180ed528aff550b5215c5ebef4d
sashman <sashman90@gmail.com>
2020-01-01 23:14:59 +0000

    Test pg schema to be tested against and a basic test to call plsm to generate the schema files in the lib directory to test compilation

--- test/plsm_test.exs
+++ test/plsm_test.exs
@@ -1,3 +1,24 @@
 defmodule PlsmTest do
   use ExUnit.Case
+
+  @schema_dir "lib/test_temp/schemas/"
+
+  describe "plsm task using postgres" do
+    setup do
+      Application.put_env(:plsm, :server, "localhost")
+      Application.put_env(:plsm, :port, "5432")
+      Application.put_env(:plsm, :database_name, "postgres")
+      Application.put_env(:plsm, :username, "postgres")
+      Application.put_env(:plsm, :password, "postgres")
+      Application.put_env(:plsm, :type, :postgres)
+      Application.put_env(:plsm, :module_name, "PlsmTest")
+      Application.put_env(:plsm, :destination, @schema_dir)
+
+      :ok
+    end
+
+    test "schema files are generated" do
+      Mix.Tasks.Plsm.run([])
+    end
+  end
 end
